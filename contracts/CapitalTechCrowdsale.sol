cddd5f1de14bc2afa40f5ae9435de7328e2f313c
Ionut Moraru <morion4000@gmail.com>
2018-06-09 14:15:35 +0300

    Major design updates and more tests

--- contracts/CapitalTechCrowdsale.sol
+++ contracts/CapitalTechCrowdsale.sol
@@ -7,32 +7,34 @@ import './CALLGToken.sol';
 import './CALLToken.sol';
 import 'openzeppelin-solidity/contracts/crowdsale/distribution/utils/RefundVault.sol';
 contract FiatContract {
-    function USD(uint _id) public constant returns (uint256);
+  function USD(uint _id) public constant returns (uint256);
 }
-contract CapitalTechCrowdsale is Ownable{
+contract CapitalTechCrowdsale is Ownable {
   using SafeMath for uint256;
   ERC20 public token_call;
   ERC20 public token_callg;
   FiatContract public fiat_contract;
   RefundVault public vault;
+  enum stages { PRIVATE_SALE, PRE_SALE, MAIN_SALE_1, MAIN_SALE_2, MAIN_SALE_3, MAIN_SALE_4 }
   address public wallet;
   uint256 public maxContributionPerAddress;
-  uint256 public startTime;
-  uint256 public endTime;
+  uint256 public stageStartTime;
   uint256 public weiRaised;
-  uint256 public sale_period;
   uint256 public minInvestment;
-  bool public sale_state = false;
-  string public stage;
+  stages public stage;
+  bool public is_finalized;
   mapping(address => uint256) public contributions;
+  mapping(uint => uint) public stages_duration;
   uint256 public callSoftCap;
   uint256 public callgSoftCap;
-  uint256 public callHardCap;
-  uint256 public callgHardCap;
   uint256 public callDistributed;
   uint256 public callgDistributed;
   event TokenPurchase(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount_call, uint256 amount_callg);
+  event TokenTransfer(address indexed purchaser, address indexed beneficiary, uint256 value, uint256 amount_call, uint256 amount_callg);
   event Finalized();
+  function () external payable {
+    buyTokens(msg.sender);
+  }
   constructor(address _wallet, address _fiatcontract, ERC20 _token_call, ERC20 _token_callg) public {
     require(_token_call != address(0));
     require(_token_callg != address(0));
@@ -44,126 +46,157 @@ contract CapitalTechCrowdsale is Ownable{
     fiat_contract = FiatContract(_fiatcontract);
     vault = new RefundVault(_wallet);
   }
-  function getUserContribution(address _beneficiary) public view returns (uint256) {
-    return contributions[_beneficiary];
-  }
-  function calculateRate(uint256 _amount) public view returns(uint256) {
-    uint256 tokenPrice = fiat_contract.USD(0);
-    if(startTime.add(15 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(50).div(10 ** 8);
-    } else if(startTime.add(45 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(70).div(10 ** 8);
-    } else if(startTime.add(52 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(80).div(10 ** 8);
-    } else if(startTime.add(59 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(90).div(10 ** 8);
-    } else if(startTime.add(66 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(100).div(10 ** 8);
-    } else {
-        tokenPrice = tokenPrice.mul(35).div(10 ** 8);
-    }
-    return _amount.div(tokenPrice).mul(10 ** 10);
-  }
-  function () external payable {
-    buyTokens(msg.sender);
-  }
-  function compareStages (string a, string b) internal pure returns (bool){
-       return keccak256(abi.encodePacked(a)) == keccak256(abi.encodePacked(b));
-  }
-  function buyTokens(address _beneficiary) public payable {
-    uint256 weiAmount = msg.value;
-    _preValidatePurchase(_beneficiary, weiAmount);
-    uint256 call_tokens = _getTokenAmount(weiAmount);
-    uint256 callg_tokens = call_tokens.mul(200);
-    _postValidatePurchase(call_tokens, callg_tokens);
-    weiRaised = weiRaised.add(weiAmount);
-    callDistributed = callDistributed.add(call_tokens);
-    callgDistributed = callDistributed.add(callg_tokens);
-    _processPurchase(_beneficiary, call_tokens, callg_tokens);
-    emit TokenPurchase(msg.sender, _beneficiary, weiAmount, call_tokens, callg_tokens);
-    _updatePurchasingState(_beneficiary, weiAmount);
-    _forwardFunds();
-  }
-  function _getTokenAmount(uint256 _weiAmount) internal view returns (uint256) {
-    uint256 tokenPrice = fiat_contract.USD(0);
-    if(startTime.add(15 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(50).div(10 ** 8);
-    } else if(startTime.add(45 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(70).div(10 ** 8);
-    } else if(startTime.add(52 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(80).div(10 ** 8);
-    } else if(startTime.add(59 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(90).div(10 ** 8);
-    } else if(startTime.add(66 days) >= block.timestamp) {
-        tokenPrice = tokenPrice.mul(100).div(10 ** 8);
-    } else {
-        tokenPrice = tokenPrice.mul(35).div(10 ** 8);
-    }
-    return _weiAmount.div(tokenPrice).mul(10 ** 10);
-  }
-  function hasEnded() public view returns (bool) {
-    require(sale_state);
-    return block.timestamp > endTime;
-  }
-  function _preValidatePurchase(address _beneficiary, uint256 _weiAmount) internal {
-    require(!hasEnded());
-    require(_beneficiary != address(0));
-    require(_weiAmount >= minInvestment);
-    require(contributions[_beneficiary].add(_weiAmount) <= maxContributionPerAddress);
-  }
-  function _postValidatePurchase(uint256 _call, uint256 _callg) internal {
-    require(callDistributed.add(_call) <= callHardCap);
-    require(callgDistributed.add(_callg) <= callgHardCap);
-  }
-  function _processPurchase(address _beneficiary, uint256 _callAmount, uint256 _callgAmount) internal {
-    _deliverTokens(_beneficiary, _callAmount, _callgAmount);
-  }
-  function _deliverTokens(address _beneficiary, uint256 _callAmount, uint256 _callgAmount) internal {
-    require(MintableToken(token_call).mint(_beneficiary, _callAmount));
-    require(MintableToken(token_callg).mint(_beneficiary, _callgAmount));
-  }
-  function _updatePurchasingState(address _beneficiary, uint256 _weiAmount) internal {
-    contributions[_beneficiary] = contributions[_beneficiary].add(_weiAmount);
-  }
-  function _forwardFunds() internal {
-    vault.deposit.value(msg.value)(msg.sender);
-  }
-  function finalize() onlyOwner public {
-    emit Finalized();
-    stage = "ended";
-    sale_state = false;
-    finalization();
-  }
   function powerUpContract() public onlyOwner{
-    require(!sale_state);
-    startTime = block.timestamp;
-    sale_period = 75 days;
-    endTime = block.timestamp.add(sale_period);
-    sale_state = true;
-    stage = "private";
+    require(!is_finalized);
+    stageStartTime = block.timestamp;
+    stage = stages.PRIVATE_SALE;
     callDistributed = 0;
     callgDistributed = 0;
     weiRaised = 0;
     callSoftCap = 10710000000000000000000000;
     callgSoftCap = 2142000000000000000000000000;
-    callHardCap = 52500000000000000000000000;
-    callgHardCap = 10500000000000000000000000000;
     maxContributionPerAddress = 1500 ether;
     minInvestment = 0.01 ether;
+    is_finalized = false;
+    stages_duration[uint(stages.PRIVATE_SALE)] = 7 days;
+    stages_duration[uint(stages.PRE_SALE)] = 7 days;
+    stages_duration[uint(stages.MAIN_SALE_1)] = 7 days;
+    stages_duration[uint(stages.MAIN_SALE_2)] = 7 days;
+    stages_duration[uint(stages.MAIN_SALE_3)] = 7 days;
+    stages_duration[uint(stages.MAIN_SALE_4)] = 7 days;
   }
-  function extendCrowdsale(uint date) public onlyOwner {
-    endTime = endTime.add(date);
+  function getUserContribution(address _beneficiary) public view returns (uint256) {
+    return contributions[_beneficiary];
   }
+  function getPriceForCurrentState(uint256 _amount) public view returns(uint256) {
+    uint256 tokenPrice = fiat_contract.USD(0);
+    if(stage == stages.PRIVATE_SALE) {
+        tokenPrice = tokenPrice.mul(50).div(10 ** 8);
+    } else if(stage == stages.PRE_SALE) {
+        tokenPrice = tokenPrice.mul(70).div(10 ** 8);
+    } else if(stage == stages.MAIN_SALE_1) {
+        tokenPrice = tokenPrice.mul(80).div(10 ** 8);
+    } else if(stage == stages.MAIN_SALE_2) {
+        tokenPrice = tokenPrice.mul(90).div(10 ** 8);
+    } else if(stage == stages.MAIN_SALE_3) {
+        tokenPrice = tokenPrice.mul(100).div(10 ** 8);
+    } else if(stage == stages.MAIN_SALE_4) {
+      // TODO: What is the multiplier for MAIN_SALE_4
+      tokenPrice = tokenPrice.mul(100).div(10 ** 8);
+    } else {
+      // TODO: Why this condition?
+      tokenPrice = tokenPrice.mul(35).div(10 ** 8);
+    }
+    return _amount.div(tokenPrice).mul(10 ** 10);
+  }
+  function _getNextStage() internal view returns (stages) {
+    stages next_stage;
+    if (stage == stages.PRIVATE_SALE) {
+      next_stage = stages.PRE_SALE;
+    } else if (stage == stages.PRE_SALE) {
+      next_stage = stages.MAIN_SALE_1;
+    } else if (stage == stages.MAIN_SALE_1) {
+      next_stage = stages.MAIN_SALE_2;
+    } else if (stage == stages.MAIN_SALE_2) {
+      next_stage = stages.MAIN_SALE_3;
+    } else if (stage == stages.MAIN_SALE_3) {
+      next_stage = stages.MAIN_SALE_4;
+    } else {
+      // TODO: what do in case next stage doesn't match anything else
+      next_stage = stages.MAIN_SALE_4;
+    }
+    return next_stage;
+  }
+  function _getHardCap(stages _stage) internal view returns (uint, uint) {
+    uint hardcap_call;
+    uint hardcap_callg;
+    if (_stage == stages.PRIVATE_SALE) {
+      hardcap_call = 10710000000000000000000000;
+      hardcap_callg = 10710000000000000000000000;
+    } else if (_stage == stages.PRE_SALE) {
+      hardcap_call = 10710000000000000000000000;
+      hardcap_callg = 10710000000000000000000000;
+    } else if (_stage == stages.MAIN_SALE_1) {
+      hardcap_call = 10710000000000000000000000;
+      hardcap_callg = 10710000000000000000000000;
+    } else if (_stage == stages.MAIN_SALE_2) {
+      hardcap_call = 10710000000000000000000000;
+      hardcap_callg = 10710000000000000000000000;
+    } else if (_stage == stages.MAIN_SALE_3) {
+      hardcap_call = 10710000000000000000000000;
+      hardcap_callg = 10710000000000000000000000;
+    } else if (_stage == stages.MAIN_SALE_4) {
+      hardcap_call = 10710000000000000000000000;
+      hardcap_callg = 10710000000000000000000000;
+    } else {
+      // TODO: Should there by a default hardcap?
+      hardcap_call = 10710000000000000000000000;
+      hardcap_callg = 10710000000000000000000000;
+    }
+    return (hardcap_call, hardcap_callg);
+  }
+  function updateStage() public {
+    uint _duration = stages_duration[uint(stage)];
+    (uint _hardcapCall, uint _hardcapCallg) = _getHardCap(stage);
+    if(stageStartTime.add(_duration) >= block.timestamp || callDistributed >= _hardcapCall || callgDistributed >= _hardcapCallg) {
+      stages next_stage = _getNextStage();
+      if (next_stage != stages.MAIN_SALE_4) {
+        stage = next_stage;
+        stageStartTime = block.timestamp;
+      } else {
+        finalization();
+      }
+    }
+  }
+  function buyTokens(address _beneficiary) public payable {
+    updateStage();
+    require(!is_finalized);
+    if (_beneficiary == address(0)) {
+      _beneficiary = msg.sender;
+    }
+    uint256 weiAmount = msg.value;
+    require(weiAmount > 0);
+    require(_beneficiary != address(0));
+    require(weiAmount >= minInvestment);
+    require(contributions[_beneficiary].add(weiAmount) <= maxContributionPerAddress);
+    uint256 call_tokens = getPriceForCurrentState(weiAmount);
+    uint256 callg_tokens = call_tokens.mul(200);
+    //require(callDistributed.add(call_tokens) <= callHardCap);
+    //require(callgDistributed.add(callg_tokens) <= callgHardCap);
+    weiRaised = weiRaised.add(weiAmount);
+    callDistributed = callDistributed.add(call_tokens);
+    callgDistributed = callDistributed.add(callg_tokens);
+    MintableToken(token_call).mint(_beneficiary, call_tokens);
+    MintableToken(token_callg).mint(_beneficiary, callg_tokens);
+    emit TokenPurchase(msg.sender, _beneficiary, weiAmount, call_tokens, callg_tokens);
+    contributions[_beneficiary] = contributions[_beneficiary].add(weiAmount);
+    vault.deposit.value(msg.value)(msg.sender);
+  }
+  function finalize() onlyOwner public {
+    finalization();
+  }
+  // TODO: Allow extensions for any stage or just current?
+  // TODO: Allow extensions or updates
+  function extendPeriodForStage(uint date, stages _stage) public onlyOwner {
+    stages_duration[uint(_stage)] = stages_duration[uint(_stage)].add(date);
+  }
+  /* TODO: refactor function */
   function transferTokens(address _to, uint256 _amount) public onlyOwner {
+    updateStage();
+    require(!is_finalized);
     require(_to != address(0));
     require(_amount > 0);
-    require(callDistributed.add(_amount) <= callHardCap);
-    require(callgDistributed.add(_amount.mul(200)) <= callgHardCap);
-    require(MintableToken(token_call).mint(_to, _amount));
-    require(MintableToken(token_callg).mint(_to, _amount.mul(200)));
+    // TODO: Can the owner create tokens even after the sale is over, or even over the hardcap is reached
+    //require(callDistributed.add(_amount) <= callHardCap);
+    //require(callgDistributed.add(_amount.mul(200)) <= callgHardCap);
+    callDistributed = callDistributed.add(_amount);
+    callgDistributed = callgDistributed.add(_amount.mul(200));
+    MintableToken(token_call).mint(_to, _amount);
+    MintableToken(token_callg).mint(_to, _amount.mul(200));
+    emit TokenTransfer(msg.sender, _to, _amount, _amount, _amount.mul(200));
   }
   function claimRefund() public {
-    require(!sale_state);
+    require(is_finalized);
     require(!goalReached());
     vault.refund(msg.sender);
   }
@@ -171,11 +204,10 @@ contract CapitalTechCrowdsale is Ownable{
     require(callDistributed >= callSoftCap);
     require(callgDistributed >= callgSoftCap);
   }
-  function hardcapReached() public returns (bool) {
-    require(callDistributed >= callHardCap);
-    require(callgDistributed >= callgHardCap);
-  }
   function finalization() internal {
+    require(!is_finalized);
+    is_finalized = true;
+    emit Finalized();
     if (goalReached()) {
       vault.close();
     } else {
--- test/TestCrowdsale.js
+++ test/TestCrowdsale.js
@@ -2,10 +2,7 @@ import ether from './helpers/ether';
 import {
   advanceBlock
 } from './helpers/advanceToBlock.js';
-import {
-  increaseTimeTo,
-  duration
-} from './helpers/increaseTime.js';
+import increaseTime from './helpers/increaseTime.js';
 import latestTime from './helpers/latestTime.js';
 import EVMRevert from './helpers/EVMRevert.js';
 
@@ -38,10 +35,7 @@ contract("CapitalTechCrowdsale", function([owner, wallet, investor, otherInvesto
     this.call_token.should.exist;
     this.callg_token.should.exist;
 
-    const startTime = await this.crowdsale.startTime();
-    const endTime = await this.crowdsale.endTime();
-    const sale_period = await this.crowdsale.sale_period();
-    const sale_state = await this.crowdsale.sale_state();
+    const stageStartTime = await this.crowdsale.stageStartTime();
     const stage = await this.crowdsale.stage();
     const callDistributed = await this.crowdsale.callDistributed();
     const callgDistributed = await this.crowdsale.callgDistributed();
@@ -50,11 +44,8 @@ contract("CapitalTechCrowdsale", function([owner, wallet, investor, otherInvesto
     const walletCrowdsale = await this.crowdsale.wallet();
     const weiRaised = await this.crowdsale.weiRaised();
 
-    startTime.toNumber().should.be.greaterThan(0);
-    endTime.toNumber().should.be.greaterThan(0);
-    sale_period.toNumber().should.be.equal(6480000); // 75 days
-    sale_state.should.be.equal(true);
-    stage.should.be.equal('private');
+    stageStartTime.toNumber().should.be.greaterThan(0);
+    stage.toNumber().should.be.equal(0);
     callDistributed.toNumber().should.be.equal(0);
     callgDistributed.toNumber().should.be.equal(0);
     maxContributionPerAddress.div(1e18).toNumber().should.be.equal(1500);
@@ -63,18 +54,11 @@ contract("CapitalTechCrowdsale", function([owner, wallet, investor, otherInvesto
     weiRaised.toNumber().should.be.equal(0);
   });
 
-  it("The crowdsale should be started", async function() {
-    // let instance = await CapitalTechCrowdsale.deployed();
-    let ended = await this.crowdsale.hasEnded.call();
-
-    assert.equal(ended, false, "The crowdsale has started");
-  });
-
-  it("The investor should be able to buy tokens", async function() {
+  it("The investor should be able to buy tokens in PRIVATE_SALE", async function() {
     // TODO: Use the contract instance created during beforeEach
     let instance = await CapitalTechCrowdsale.deployed();
-    // TODO: Can someone buy tokens in behalf of someone else?
-    let purchase = await instance.buyTokens(investor, {from: investor, value: 100000000000000000});
+    let amount = new BigNumber(0.1).mul(1e18);
+    let purchase = await instance.buyTokens(investor, {from: investor, value: amount});
 
     // TODO: Check more information on the transaction
     purchase.logs.length.should.be.equal(1);
@@ -84,10 +68,42 @@ contract("CapitalTechCrowdsale", function([owner, wallet, investor, otherInvesto
     const weiRaised = await instance.weiRaised();
     const userContribution = await instance.getUserContribution(investor);
 
-    callDistributed.div(1e18).toNumber().should.be.greaterThan(118);
-    callgDistributed.div(1e18).toNumber().should.be.greaterThan(23895);
+    callDistributed.div(1e18).toNumber().should.be.equal(84.91472141);
+    callgDistributed.div(1e18).toNumber().should.be.equal(17067.85900341);
     weiRaised.div(1e18).toNumber().should.be.equal(0.1);
-    userContribution.toNumber().should.be.equal(0.1);
+    userContribution.div(1e18).toNumber().should.be.equal(0.1);
     // TODO: Check vault balance
+  });
+
+  it("It should increase time by 7 days", async function() {
+    var duration = 7 * 60 * 60 * 24;
+
+    increaseTime(duration);
+  });
+
+  it("The stage should advance to PRE_SALE", async function() {
+    // TODO: Use the contract instance created during beforeEach
+    let instance = await CapitalTechCrowdsale.deployed();
+    let call = await instance.updateStage()
+
+    const stage = await instance.stage();
+
+    stage.toNumber().should.be.equal(1);
+  });
+
+  it("It should increase time by 7 days", async function() {
+    var duration = 7 * 60 * 60 * 24;
+
+    increaseTime(duration);
+  });
+
+  it("The stage should advance to MAIN_SALE_1", async function() {
+    // TODO: Use the contract instance created during beforeEach
+    let instance = await CapitalTechCrowdsale.deployed();
+    let call = await instance.updateStage()
+
+    const stage = await instance.stage();
+
+    stage.toNumber().should.be.equal(2);
   });
 });
