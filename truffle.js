f0ebc3232452cbc9d5b8a00aee55a5945d9185cb
Ionut Moraru <morion4000@gmail.com>
2018-06-22 13:57:16 +0300

    Updated truffle.js

--- truffle.js
+++ truffle.js
@@ -18,7 +18,18 @@ module.exports = {
       { test: /\.sol/, loader: 'truffle-solidity' }
     ]
   },
-  networks: {}
+  networks: {
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777",
+    },
+    test: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "5777",
+    }
+  }
 };
 
 for (let networkName in NETWORK_IDS) {
